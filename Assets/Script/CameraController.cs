bf7ef6ca860d3b3cdb704205c9bebcd6c5b014f2
Anel245 <bretillonlisa@gmail.com>
2022-04-12 10:59:43 +0200

    Goal/End Trigger
    -Once you collide with the prefab END a "Game Complete" screen appears with a little animation

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,15 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    public GameObject target;
-    public float xOffset, yOffset, zOffset;
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = target.transform.position + new Vector3(xOffset, yOffset, zOffset);
-        transform.LookAt(target.transform.position);
-    }
-}
+//public class CameraController : MonoBehaviour
+//{
+//    public GameObject target;
+//    //public float xOffset, yOffset, zOffset;
+//    public float Horizontal_Input;
+//    public float Vertical_Input;
+    
+//    void Update()
+//    {
+//        transform.position = target.transform.position + new Vector3(Horizontal_Input, 0, Vertical_Input);
+//        transform.LookAt(target.transform.position);
+//    }
+//    public void OnMovement(InputAction.CallbackContext context)
+//    {
+//        Horizontal_Input = context.ReadValue<Vector2>().x;
+//        Vertical_Input = context.ReadValue<Vector2>().y;
+//    }
+
+//}
