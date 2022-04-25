3bb1eaa93b44f7304057048838f87c7020afbbf4
Anel245 <bretillonlisa@gmail.com>
2022-04-25 16:14:19 +0200

    Fixed Checkpoints

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+  
+
+
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.transform.tag == "Player") 
--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public static Vector3 lastCheckPointPos = new Vector3(-36, 1, -40);
-   
+    public GameObject Player;
+    void Update()
+    {
+        if (Player.transform.position.y < -2f)
+        {
+            Debug.Log("falling");
+            Player.transform.position = lastCheckPointPos;
+            Debug.Log(lastCheckPointPos);
+            Debug.Log(Player.transform.position);
+        }
+    }
 
     private void Awake()
     {
-        GameObject.FindGameObjectsWithTag("Player").transform.position = lastCheckPointPos;
+        GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos;
     }
+
 }
