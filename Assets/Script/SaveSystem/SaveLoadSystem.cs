be472af10d7e692e83f803d0677d5040705e5bc9
Anel245 <bretillonlisa@gmail.com>
2022-04-26 15:32:42 +0200

    Basic SaveSystem
    -Basic Save and Load System
    -LisaTestScene hierarchy cleaning

--- Assets/Script/SaveSystem/SaveLoadSystem.cs
+++ Assets/Script/SaveSystem/SaveLoadSystem.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class SaveLoadSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string SavePath => $"{Application.persistentDataPath}/save.txt";
+
+    [ContextMenu("Save")]
+    void Save()
     {
-        
+        var state = LoadFile();
+        SaveState(state);
+        SaveFile(state);
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("Load")]
+    void Load()
     {
-        
+        var state = LoadFile();
+        LoadState(state);
     }
+
+    public void SaveFile(object state)
+    {
+        using (var stream = File.Open(SavePath, FileMode.Create))
+        {
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, state);
+        }
+    }
+
+    Dictionary<string, object> LoadFile()
+    {
+        if (!File.Exists(SavePath))
+        {
+            Debug.Log("No save file found");
+            return new Dictionary<string, object>();
+        }
+        using (FileStream stream = File.Open(SavePath, FileMode.Open))
+        {
+            var formatter = new BinaryFormatter();
+            return (Dictionary<string, object>)formatter.Deserialize(stream);
+        }
+    }
+
+    void SaveState(Dictionary<string, object> state)
+    {
+        foreach(var saveable in FindObjectsOfType<SaveableEntity>())
+        {
+            state[saveable.Id] = saveable.SaveState();
+        }
+    }
+
+    void LoadState(Dictionary<string, object> state)
+    {
+        foreach (var saveable in FindObjectsOfType<SaveableEntity>())
+        {
+            if(state.TryGetValue(saveable.Id, out object savedState))
+            {
+                saveable.LoadState(savedState);
+            }
+        }
+    }
+
+
+
+
 }
