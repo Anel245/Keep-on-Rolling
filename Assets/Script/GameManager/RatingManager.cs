31d67ea7d222badaafa19c12bcf984670d06a659
Anel245 <bretillonlisa@gmail.com>
2022-07-07 16:04:25 +0200

    Current Script State

--- Assets/Script/GameManager/RatingManager.cs
+++ Assets/Script/GameManager/RatingManager.cs
@@ -5,6 +5,8 @@
 //public class RatingManager : MonoBehaviour
 //{
 //    public Score Scorescript;
+//    public TimerManager Timerscript;
+
 
 //    [Header("Time Values")]
 //    public float GoodTime;
@@ -17,57 +19,88 @@
 //    public float BadScore;
 
 //    [Header("Stars")]
-//    public GameObject 
+//    public GameObject HalfStarLeft_1;
+//    public GameObject HalfStarRight_1;
+//    public GameObject HalfStarLeft_2;
+//    public GameObject HalfStarRight_2;
+//    public GameObject HalfStarLeft_3;
+//    public GameObject HalfStarRight_3;
+//    public GameObject NoStars;
 
 //    private void Awake()
 //    {
 //        Scorescript = GameObject.Find("Score").GetComponent<Score>();
+//        Timerscript = GameObject.Find("Timemanager").GetComponent<TimerManager>();
+
 //    }
 //    void Start()
 //    {   // Good Time and Good Score
-//        if (Time < GoodTime && Score.theScore > GoodScore)
+//        if (TimerManager.startTime < GoodTime && Score.theScore > GoodScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 3 Stars
+//            HalfStarRight_2.SetActive(true);
+//            HalfStarLeft_3.SetActive(true);
+//            HalfStarRight_3.SetActive(true);
 //        }
 //        // Good Time and Middle Score
 //        if (Time < GoodTime && Score.theScore > MiddleScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 2 Stars and a Half
+//            HalfStarRight_2.SetActive(true);
+//            HalfStarLeft_3.SetActive(true);
 //        }
 //        // Good Time and Bad Score
 //        if (Time < GoodTime && Score.theScore > BadScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 2 Stars
+//            HalfStarRight_2.SetActive(true);
 //        }
 //        // Middle Time and Good Score
 //        if (Time < MiddleTime && Score.theScore > GoodScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 2 Stars and a Half
+//            HalfStarRight_2.SetActive(true);
+//            HalfStarLeft_3.SetActive(true);
 //        }
 //        // Middle Time and Middle Score
 //        if (Time < MiddleTime && Score.theScore > MiddleScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 1 Star and a Half
 //        }
 //        // Middle Time and Bad Score
 //        if (Time < MiddleTime && Score.theScore > BadScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);               // 1 Star 
 //        }
 //        // Bad Time and Good Score
 //        if (Time < BadTime && Score.theScore > GoodScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);
+//            HalfStarLeft_2.SetActive(true);                // 2 Stars
+//            HalfStarRight_2.SetActive(true);
 //        }
 //        // Bad Time and Middle Score
 //        if (Time < BadTime && Score.theScore > MiddleScore)
 //        {
-
+//            HalfStarLeft_1.SetActive(true);
+//            HalfStarRight_1.SetActive(true);               // 1 Star 
 //        }
 //        // Bad Time and Bad Score
 //        if (Time < BadTime && Score.theScore > BadScore)
 //        {
-
+//            NoStars.SetActive(true);                     // No Stars
 //        }
 //    }
 
--- Assets/Script/GameManager/TimerManager.cs
+++ Assets/Script/GameManager/TimerManager.cs
@@ -9,12 +9,15 @@ public class TimerManager : MonoBehaviour
     public TextMeshProUGUI TimerText;
     public TextMeshProUGUI highScore;
     private float startTime;
+    private float EndTime;
     private bool finnished = false;
     
 
     void Start()
     {
         startTime = Time.time;
+        
+        
         // Display the HighScore
         //highScore.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
     }
@@ -22,15 +25,16 @@ public class TimerManager : MonoBehaviour
     {
         if (finnished)
         {
-            // If the time that the player took to finish the level is lower as was it is save in the highScore then...
-            if (startTime < PlayerPrefs.GetFloat("HighScore", 0))
-            {
-              Debug.Log("better time");
-              // The highscore is set here
-              PlayerPrefs.SetFloat("HighScore", startTime);
-              // Update the highscore
-              highScore.text = startTime.ToString();
-            }
+            //// If the time that the player took to finish the level is lower as was it is save in the highScore then...
+            //if (startTime < PlayerPrefs.GetFloat("HighScore", 0))
+            //{
+            //  Debug.Log("better time");
+            //  // The highscore is set here
+            //  PlayerPrefs.SetFloat("HighScore", startTime);
+            //  // Update the highscore
+            //  highScore.text = startTime.ToString();
+            //}
+            
             return;
         }
 
