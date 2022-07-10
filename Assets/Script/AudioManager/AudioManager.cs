b02db052cbef676476b207cece9a65bf1c33ecb7
Héctor Jotta <hectorjotta@gmail.com>
2022-07-10 22:16:16 +0200

    AudioManager update + Button_Click int

--- Assets/Script/AudioManager/AudioManager.cs
+++ Assets/Script/AudioManager/AudioManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    public static AudioManager instance = null;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void Button_Click()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Button_Click");        
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
